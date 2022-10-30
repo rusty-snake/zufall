79556e122d95f66b34897db8133a7a0e7be2a0ca
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2022-10-30 16:46:50 +0100

    Remove cli mode

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,8 @@
-use clap::{Arg, Command};
 use gtk::prelude::*;
 use gtk::Builder;
 use rand::{thread_rng, Rng};
-use std::process::exit;
 
 fn main() {
-    let matches = Command::new("Zufall")
-        .version("3.2.0-rolling")
-        .about("Zufall, a tool to help on decisions.")
-        .subcommand(
-            Command::new("cli")
-                .about("Use zufall in the shell")
-                .arg(
-                    Arg::new("FROM")
-                        .help("lower limit of the number range")
-                        .index(1)
-                        .default_value("0"),
-                )
-                .arg(
-                    Arg::new("TO")
-                        .help("upper limit of the number range")
-                        .index(2)
-                        .default_value("1"),
-                ),
-        )
-        .subcommand(Command::new("gui").about("Start in graphical mode"))
-        .get_matches();
-
-    match matches.subcommand_name() {
-        Some("cli") => {
-            let sc_matches = matches.subcommand_matches("cli").unwrap();
-            let value_from: u128 = sc_matches
-                .get_one::<String>("FROM")
-                .map(|s| s.as_str())
-                .unwrap()
-                .parse()
-                .expect("Failed to parse '<FROM>'");
-            let value_to: u128 = sc_matches
-                .get_one::<String>("TO")
-                .map(|s| s.as_str())
-                .unwrap()
-                .parse()
-                .expect("Failed to parse '<TO>'");
-
-            cli(
-                value_from,
-                value_to
-                    .checked_add(1)
-                    .expect("Overflow, '<TO>' is to large"),
-            );
-        }
-        Some("gui") => gui(),
-        _ => println!("Please specify a subcommand (gui or cli). See: zufall --help"),
-    };
-}
-
-fn cli(from: u128, to: u128) {
-    if from >= to {
-        println!("<FROM> must be smaller than <TO>");
-        exit(50);
-    }
-    let output = thread_rng().gen_range(from..to);
-    println!("random number: {}", output);
-}
-
-fn gui() {
     gtk::init().expect("Failed to initialize gtk");
 
     let builder = Builder::from_string(include_str!("zufall.glade"));
